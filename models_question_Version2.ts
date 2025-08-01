78118d9b2897937b0152cb99dd5193a37df69b9b
exam007 <narongtorn.s@attorney285.co.th>
2025-08-01 16:44:54 +0700

    Delete models_question_Version2.ts

--- models_question_Version2.ts
+++
@@ -1,8 +0,0 @@
-export interface Question {
-  _id?: string;
-  examId: string;
-  question: string;
-  options: string[];
-  answer: number; // index ของตัวเลือกที่ถูกต้อง
-  explanation?: string;
-}
